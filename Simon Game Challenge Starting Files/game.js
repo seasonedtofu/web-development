11bf42a53b1c9b7921d135d57de8477f729366a7
seasonedtofu <seasonedtofu@gmail.com>
2021-09-08 01:56:01 -0400

    simon says update

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,22 @@
+var gamePattern = [];
+var buttonColours = ["red","blue","green","yellow"];
+var userClickedPattern = [];
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+};
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+});
+
+function playSound(name){
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+};
